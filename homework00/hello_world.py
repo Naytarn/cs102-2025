b4ece023d6c1295e8a5a313fda92bfdf77053ad1
Naytarn <agatha.zhe@gmail.com>
2025-10-07 11:46:15 +0300

    black applied to program

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,9 +1,10 @@
-'''
+"""
 Corrected the mistake. Now the file outputs a string, as expected.
-'''
+"""
+
 
 def text():
-    '''
+    """
     Function returns "message" string when called.
-    '''
+    """
     return "message"

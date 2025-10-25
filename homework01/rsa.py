dbaea7a1d33e719e2c1ab81ef54db95456a7c3e6
Naytarn <agatha.zhe@gmail.com>
2025-10-25 14:38:02 +0300

    Реализована функция is_prime

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,7 +1,6 @@
 import random
 import typing as tp
 
-
 def is_prime(n: int) -> bool:
     """
     Tests to see if a number is prime.
@@ -12,7 +11,14 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
+    i = 2
+    if n <= 1:
+        return False
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
     pass
 
 

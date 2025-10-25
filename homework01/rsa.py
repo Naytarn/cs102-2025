7e86ef6cbce7401680f75afe2962f6c463f2ff10
Naytarn <agatha.zhe@gmail.com>
2025-10-25 19:34:24 +0300

    Реализована функция gcd

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -30,7 +30,10 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
     pass
 
 
@@ -51,18 +54,16 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
-    # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
 
     # Use Euclid's Algorithm to verify that e and phi(n) are coprime
     g = gcd(e, phi)
-    while g != 1:
+    while g != 1 or not is_prime(e):
         e = random.randrange(1, phi)
         g = gcd(e, phi)
 

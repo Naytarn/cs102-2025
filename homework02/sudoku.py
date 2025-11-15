a6ad5bb8e4860819ebd8bfbe0faf966ec8ddf51b
Naytarn <agatha.zhe@gmail.com>
2025-11-15 19:44:33 +0300

    get_block() function implemented

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -79,7 +79,12 @@ def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[s
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    pass
+
+    block_row = (pos[0] // 3) * 3
+    block_col = (pos[1] // 3) * 3
+
+    return [grid[i][j] for i in range(block_row, block_row + 3)
+            for j in range(block_col, block_col + 3)]
 
 
 def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[int, int]]:

d710da866b23704a6c255efba32f74f22cac7913
Naytarn <agatha.zhe@gmail.com>
2025-12-01 18:15:11 +0300

    this time actually added the manual door coords

--- homework03/maze.py
+++ homework03/maze.py
@@ -70,8 +70,8 @@ def bin_tree_maze(
         y_in = randint(0, cols - 1) if x_in in (0, rows - 1) else choice((0, cols - 1))
         y_out = randint(0, cols - 1) if x_out in (0, rows - 1) else choice((0, cols - 1))
     else:
-        x_in, y_in = 0, cols - 2
-        x_out, y_out = rows - 1, 1
+        x_in, y_in = [int(i) for i in input("Please enter the first door's coordinates:").split()]
+        x_out, y_out = [int(i) for i in input("Please enter the second door's coordinates:").split()]
 
     grid[x_in][y_in], grid[x_out][y_out] = "X", "X"
 

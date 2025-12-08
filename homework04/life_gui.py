4d73817d57c97e8d8c342e29a27e61be07e247c2
Naytarn <agatha.zhe@gmail.com>
2025-12-08 20:17:41 +0300

    reformatted

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -1,10 +1,19 @@
+"""visualizes game with pygame!"""
+
+# pylint: disable=line-too-long
+# pylint: disable=E1101
+
+
 import pygame
-from life import GameOfLife
 from pygame.locals import *
+
+from life import GameOfLife
 from ui import UI
 
 
 class GUI(UI):
+    """very scary class"""
+
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
         super().__init__(life)
         self.cell_size = cell_size
@@ -15,7 +24,7 @@ class GUI(UI):
         self.screen = pygame.display.set_mode((self.width, self.height))
 
     def draw_lines(self) -> None:
-        """ Отрисовать сетку """
+        """Отрисовать сетку"""
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
@@ -23,29 +32,32 @@ class GUI(UI):
 
     def draw_grid(self) -> None:
         """
-                Отрисовка списка клеток с закрашиванием их в соответствующе цвета.
-                """
+        Отрисовка списка клеток с закрашиванием их в соответствующе цвета.
+        """
         window = self.screen
 
         for i, row in enumerate(self.life.curr_generation):
             for j, cell in enumerate(row):
                 if cell == 1:
-                    cell_color = 'lavender'
+                    cell_color = "lavender"
                 else:
-                    cell_color = 'grey'
+                    cell_color = "grey"
 
                 cell_shape = pygame.Rect(j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size)
                 pygame.draw.rect(window, cell_color, cell_shape)
 
     def set_cell_state(self, click_pos: tuple) -> None:
+        """changes cell state on click"""
         click_y, click_x = click_pos
         cell_chosen_x = click_x // self.cell_size
         cell_chosen_y = click_y // self.cell_size
 
-        self.life.curr_generation[cell_chosen_x][cell_chosen_y] = 1 - self.life.curr_generation[cell_chosen_x][cell_chosen_y]
+        self.life.curr_generation[cell_chosen_x][cell_chosen_y] = (
+            1 - self.life.curr_generation[cell_chosen_x][cell_chosen_y]
+        )
 
     def run(self) -> None:
-        """ Запустить игру """
+        """Запустить игру"""
         clock = pygame.time.Clock()
         pygame.display.set_caption("Game of Life")
         self.screen.fill(pygame.Color("white"))
@@ -75,6 +87,6 @@ class GUI(UI):
 
 
 # run game
-life = GameOfLife((48, 64))
-gui = GUI(life)
+life_game = GameOfLife((48, 64))
+gui = GUI(life_game)
 gui.run()

0be9bb36145777db5c192536df95bd561f590ff3
Naytarn <agatha.zhe@gmail.com>
2025-12-08 16:37:49 +0300

    it works!

--- homework04/life_console.py
+++ homework04/life_console.py
@@ -1,5 +1,5 @@
 import curses
-
+import time
 from life import GameOfLife
 from ui import UI
 
@@ -10,13 +10,40 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        height, width = self.life.rows, self.life.cols
+        for x in range(1, width + 1):
+            screen.addch(0, x, '_')
+            screen.addch(height, x, '_')
+        for y in range(1, height + 1):
+            screen.addch(y, 0, '|')
+            screen.addch(y, width, '|')
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        grid = self.life.curr_generation
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if cell == 1:
+                    screen.addch(i + 1, j + 1, '■')
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        curses.noecho()
+        curses.cbreak()
+        screen.keypad(True)
+        curses.curs_set(0)
+        screen.nodelay(True)
+
+        while self.life.is_changing and not self.life.is_max_generations_exceeded:
+            screen.clear()
+            self.draw_borders(screen)
+            self.draw_grid(screen)
+            screen.refresh()
+            time.sleep(0.1)
+            self.life.step()
         curses.endwin()
+
+
+life = GameOfLife((24, 80), max_generations=50)
+ui = Console(life)
+ui.run()
